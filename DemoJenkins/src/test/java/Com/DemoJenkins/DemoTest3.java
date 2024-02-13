ef6e5571474bacfbb478029f56815d2080ed1b7d
hp <hp@LAPTOP-L9D1A95H>
2024-02-13 15:13:14 +0530

    Eng2 commit

---
+++ DemoJenkins/src/test/java/Com/DemoJenkins/DemoTest3.java
@@ -0,0 +1,13 @@
+package Com.DemoJenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest3 {
+	@Test
+	public void m3()
+	{
+		Reporter.log("TC3 is passed",true);
+	}
+
+}
